dfccfcacd7a308527b0089dff6d0da85538981d9
Behnaz <bearzani@microsoft.com>
2022-05-10 13:42:04 -0700

    added more test cases, fixed a few bugs, read and commented on parts of ryan's code

--- MetaOptimize/MetaOptimize.Test/KktOptimizationTests.cs
+++ MetaOptimize/MetaOptimize.Test/KktOptimizationTests.cs
@@ -23,16 +23,17 @@ namespace MetaOptimize.Test
             var solver = new SolverZen();
             var x = solver.CreateVariable("x");
             var y = solver.CreateVariable("y");
-
             var encoder = new KktOptimizationGenerator<Zen<Real>, ZenSolution>(solver, new HashSet<Zen<Real>>() { x, y }, new HashSet<Zen<Real>>());
 
+            // Add constraints that are equalities: format has to be all variables and non-zero constants
+            // to the left.
             // x + 2y == 10
+
             encoder.AddEqZeroConstraint(new Polynomial<Zen<Real>>(new Term<Zen<Real>>(1, x), new Term<Zen<Real>>(2, y), new Term<Zen<Real>>(-10)));
 
             // x >= 0, y>= 0
             encoder.AddLeqZeroConstraint(new Polynomial<Zen<Real>>(new Term<Zen<Real>>(-1, x)));
             encoder.AddLeqZeroConstraint(new Polynomial<Zen<Real>>(new Term<Zen<Real>>(-1, y)));
-
             // maximize y - x
             encoder.AddMaximizationConstraints(new Polynomial<Zen<Real>>(new Term<Zen<Real>>(1, y), new Term<Zen<Real>>(-1, x)));
 
