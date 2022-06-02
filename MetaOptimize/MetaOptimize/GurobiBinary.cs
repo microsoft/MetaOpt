3f78463988ab48689ba1171e8c98cf61474ad811
Behnaz <bearzani@microsoft.com>
2022-06-02 10:56:41 -0700

    added clean version of sovler

---
+++ MetaOptimize/MetaOptimize/GurobiBinary.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Gurobi;
+
+namespace MetaOptimize
+{
+    /// <summary>
+    /// Implements a sanitized version of SolverGurobi.
+    /// </summary>
+    public class GurobiBinary : GurobiSOS
+    {
+        private double _bigM = GRB.MAXINT;
+        private double _tolerance = Math.Pow(10, -8);
+
+        /// <summary>
+        /// Wrapper that convers the new types to guroubi types and then
+        /// calls the proper function.
+        /// </summary>
+        /// <param name="polynomial1"></param>
+        /// <param name="polynomial2"></param>
+        public override void AddOrEqZeroConstraint(Polynomial<GRBVar> polynomial1, Polynomial<GRBVar> polynomial2)
+        {
+            GRBLinExpr poly1 = this.Convert(polynomial1);
+            GRBLinExpr poly2 = this.Convert(polynomial2);
+            GRBLinExpr poly2Neg = this.Convert(polynomial2.Negate());
+
+            var alpha = this._model.AddVar(0.0, 1.0, 0.0, GRB.BINARY, "binary_" + this._auxiliaryVars.Count);
+            this._auxiliaryVars.Add($"binary_{this._auxiliaryVars.Count}", alpha);
+
+            poly1.AddTerm(-1 * this._bigM, alpha);
+
+            poly2.AddTerm(this._bigM, alpha);
+            poly2.AddConstant(-1 * this._bigM);
+
+            poly2Neg.AddTerm(this._bigM, alpha);
+            poly2Neg.AddConstant(-1 * this._bigM);
+
+            this._model.AddConstr(poly1, GRB.LESS_EQUAL, 0.0, "ineq_index_" + this._constraintIneqCount++);
+            this._model.AddConstr(poly2, GRB.LESS_EQUAL, 0.0, "ineq_index_" + this._constraintIneqCount++);
+            this._model.AddConstr(poly2Neg, GRB.LESS_EQUAL, 0.0, "ineq_index_" + this._constraintIneqCount++);
+        }
+        /// <summary>
+        /// Maximize the objective.
+        /// </summary>
+        /// <param name="objectiveVariable">The objective variable.</param>
+        /// <returns>A solution.</returns>
+        public override GRBModel Maximize(GRBVar objectiveVariable)
+        {
+            Console.WriteLine("in maximize call");
+            this._objective.AddTerm(1.0, objectiveVariable);
+            this._model.SetObjective(this._objective, GRB.MAXIMIZE);
+
+            this._model.Set(GRB.DoubleParam.IntFeasTol, this._tolerance);
+
+            string exhaust_dir_name = @"c:\tmp\grbsos_exhaust\rand_" + (new Random()).Next(1000) + @"\";
+            Directory.CreateDirectory(exhaust_dir_name);
+            this._model.Write($"{exhaust_dir_name}\\model_" + DateTime.Now.Millisecond + ".lp");
+
+            this._model.Optimize();
+            if (this._model.Status != GRB.Status.OPTIMAL)
+            {
+                throw new Exception($"model not optimal {ModelStatusToString(this._model.Status)}");
+            }
+
+            return this._model;
+        }
+    }
+}
