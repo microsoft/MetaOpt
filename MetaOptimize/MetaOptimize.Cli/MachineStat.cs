5b1c93a1c9face46581d0ac93bf37f5d7d83357c
PooriaNamyar <namyar@usc.edu>
2023-01-24 19:24:02 +0000

    separating server logic

---
+++ MetaOptimize/MetaOptimize.Cli/MachineStat.cs
@@ -0,0 +1,17 @@
+namespace MetaOptimize.Cli {
+    using System;
+    using System.IO;
+    /// <summary>
+    /// Machine Computation Stat.
+    /// </summary>
+    public static class MachineStat {
+        /// <summary>
+        /// num threads.
+        /// </summary>
+        public static int numThreads = 48;
+        /// <summary>
+        /// num processors.
+        /// </summary>
+        public static int numProcessors = 48;
+    }
+}
