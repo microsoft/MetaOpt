6d2823f9a831861d6072c1d46a1a7e487daef6ed
Solal Pirelli <t-spirelli@microsoft.com>
2023-07-19 13:40:55 -0700

    Add tests; KKT fails, mostly

--- MetaOptimize/MetaOptimize/GurobiSOS.cs
+++ MetaOptimize/MetaOptimize/GurobiSOS.cs
@@ -875,7 +875,8 @@ namespace MetaOptimize
             {
                 string exhaust_dir_name = @"../logs/grbsos_exhaust/rand_" + (new Random()).Next(1000) + @"/";
                 Directory.CreateDirectory(exhaust_dir_name);
-                this._model.Write($"{exhaust_dir_name}/model_feas_reduce_" + DateTime.Now.Millisecond + ".lp");
+                this._model.Write($"{exhaust_dir_name}/model_feas_reduce.lp");
+                this._model.Write($"{exhaust_dir_name}/model_feas_reduce.sol");
             }
 
             return this._model;
