9f5d67a250d4ea43a576977e28a0d2ce99dbd20c
Solal Pirelli <t-spirelli@microsoft.com>
2023-08-10 13:30:49 -0700

    Give up on mapping inputs...for now?

--- MetaOptimize/MetaOptimize.Test/DemandPinningTests.cs
+++ MetaOptimize/MetaOptimize.Test/DemandPinningTests.cs
@@ -1,7 +1,6 @@
 ﻿namespace MetaOptimize.Test
 {
     using System;
-    using Gurobi;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
@@ -53,6 +52,42 @@
             // Assert.IsTrue(TestHelper.IsApproximately(40, optimal));
             // Assert.IsTrue(TestHelper.IsApproximately(35, heuristic));
             Assert.IsTrue(Utils.IsApproximately(10, optimal - heuristic));
+        }
+
+
+        /// <summary>
+        /// Paper example
+        /// </summary>
+        [TestMethod]
+        public void TestPaperExampleTopo()
+        {
+            var topology = new Topology();
+            topology.AddNode("a");
+            topology.AddNode("b");
+            topology.AddNode("c");
+            topology.AddNode("d");
+            topology.AddNode("e");
+            topology.AddEdge("a", "b", capacity: 100);
+            topology.AddEdge("b", "c", capacity: 100);
+            topology.AddEdge("a", "d", capacity: 50);
+            topology.AddEdge("d", "e", capacity: 50);
+            topology.AddEdge("e", "c", capacity: 50);
+
+            double threshold = 50;
+            int k = 2;
+
+            // create the optimal encoder.
+            var solver = CreateSolver();
+            var optimalEncoder = new TEOptimalEncoder<TVar, TSol>(solver, k: k);
+            var heuristicEncoder = new DemandPinningEncoder<TVar, TSol>(solver, k: k, threshold: threshold);
+            var adversarialInputGenerator = new TEAdversarialInputGenerator<TVar, TSol>(topology, k: k);
+            var (optimalSolution, demandPinningSolution) = adversarialInputGenerator.MaximizeOptimalityGap(optimalEncoder, heuristicEncoder);
+
+            var optimal = optimalSolution.TotalDemandMet;
+            var heuristic = demandPinningSolution.TotalDemandMet;
+            Assert.IsTrue(TestHelper.IsApproximately(250, optimal));
+            Assert.IsTrue(TestHelper.IsApproximately(150, heuristic));
+            Assert.IsTrue(TestHelper.IsApproximately(100, optimal - heuristic));
         }
     }
 }
