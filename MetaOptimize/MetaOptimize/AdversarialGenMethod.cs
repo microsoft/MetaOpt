60dbb415efe1b8f42d164864c5b8486086280eae
PooriaNamyar <namyar@usc.edu>
2024-04-02 07:12:08 +0000

    clean up encoding names

---
+++ MetaOptimize/MetaOptimize/AdversarialGenMethod.cs
@@ -0,0 +1,13 @@
+namespace MetaOptimize
+{
+    /// <summary>
+    /// The method for generating adversarial inputs.
+    /// </summary>
+    public enum AdversarialGenMethodChoice
+    {
+        /// <summary>
+        /// use encoding.
+        /// </summary>
+        Encoding,
+    }
+}
